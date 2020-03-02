515bba0d9f998d4f0dc6c64d990fe706cab244c4
benauld <ben@portobello-consultancy.co.uk>
2020-03-02 08:50:47 +0000

    Monday morning to ensure it's recent

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 new Vue({
 	el: '#app',
 	data: {
-		age: 51,
+		a: 0,
+		b: 0,
+		age: 20,
+		available: false,
+		nearby: false,
 		x: 0,
 		y: 0,
-		name: 'Ben',
+		name: '',
 		job: 'CTO',
 		website: 'https://thenetninja.co.uk',
 		websiteTag: '<a href="https://thenetninja.co.uk">The Net Ninja</a>',
@@ -19,6 +23,24 @@ new Vue({
 		mouseMove: function(event) {
 			this.x = event.offsetX;
 			this.y = event.offsetY;
+		},
+
+		logAge: function() {
+			console.log('You entered your age');
+		},
+	},
+	computed: {
+		addToA: function() {
+			return this.a + this.age;
+		},
+		addToB: function() {
+			return this.b + this.age;
+		},
+		compClasses: function() {
+			return {
+				available: this.available,
+				nearby: this.nearby,
+			};
 		},
 	},
 });
